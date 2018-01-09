cec20c4f2debc50bb0571d8210b6815810896137
Armando Herra <armandoherra369@gmail.com>
2018-01-09 08:28:24 -0600

    Configured package.json and webpack.config.js for production. Deployed a copy of the app at armandoherra-webpackproject.surge.sh

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new WebpackDashboard()
+    new WebpackDashboard(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 };
